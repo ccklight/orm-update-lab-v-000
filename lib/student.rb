6b334f2f22b81eeb3d6ec62141e9d78a0ab79313
Cheryl K <cherylkelson@icloud.com>
2018-12-08 23:25:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,27 +31,42 @@ class Student
 
 
   def save
-      if self.id
+    if self.id
         self.update
-      else
+    else
       sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
+       INSERT INTO students (name, grade)
+       VALUES (?, ?)
     SQL
-    DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-#binding.pry
+
+   DB[:conn].execute(sql, self.name, self.grade)
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+ end
+
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    LIMIT 1
+    SQL
+
+    DB:[conn].execute(sql, name).map do|row|
+      self.new_from_db(row)
       end
-  end
-
-
-  def create
-  end
-
-  def new_from_db
-  end
-
-  def find_by_name
   end
 
   def update
